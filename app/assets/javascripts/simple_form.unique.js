297fb3bf960761c3e61ece6ff963f3702b6425b6
Peter Ragone <pcragone@gmail.com>
2015-05-21 16:33:33 -0400

    Fixes javascript reference to the correct method.

--- app/assets/javascripts/simple_form.unique.js
+++ app/assets/javascripts/simple_form.unique.js
@@ -85,7 +85,7 @@
         transformResult: function (response) {
           return typeof response === 'string' ? $.parseJSON(response) : response;
         },
-        onSearchError: getFunctionByName($(el).data('on-search-error') || 'console.log', window)
+        onSearchError: utils.getFunctionByName($(el).data('on-search-error') || 'console.log', window)
       };
 
     // Shared variables:
